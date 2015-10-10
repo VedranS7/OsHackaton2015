895a66258fe92d5330469db78a1d6a9b15fcf62c
VedranS7 <vedran.sikic@farmeron.com>
2015-10-10 13:43:56 +0200

    Updated user model

---
+++ Schegrt/WebApp/Models/DatabaseContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base("DefaultConnection") { }
+    }
+}
