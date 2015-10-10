25ad14df5970bb695a7cd71b35187c36f7ddc7cf
VedranS7 <vedran.sikic@farmeron.com>
2015-10-10 12:49:47 +0200

    Setup Student db and new password rules

--- Schegrt/WebApp/Models/IdentityModels.cs
+++ Schegrt/WebApp/Models/IdentityModels.cs
@@ -6,25 +6,29 @@ using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace WebApp.Models
 {
-    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class StudentUser : IdentityUser
     { 
+        public string Location { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<StudentUser> manager)
         {
-            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
-            // Add custom user claims here
             return userIdentity;
         }
     }
 
     public class ProviderUser : IdentityUser
     {
+        public string Location { get; set; }
+        public string CompanyName { get; set; }
+        public string URL { get; set; }
+        public string Description { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ProviderUser> manager)
         {
-            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
-            // Add custom user claims here
             return userIdentity;
         }
     }
