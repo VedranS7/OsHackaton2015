f3348249c77965a1a9f16c1b11a1273b8e3df878
Ivan Kvolik <ivan.kvolik@gmail.com>
2015-10-11 00:21:31 +0200

    Completed project matcher

--- Schegrt/WebApp/Models/Matchers/ProjectMatcher.cs
+++ Schegrt/WebApp/Models/Matchers/ProjectMatcher.cs
@@ -2,10 +2,58 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using WebApp.Models.Field;
 
 namespace WebApp.Models.Matchers
 {
 	public class ProjectMatcher
 	{
+		public ProjectMatcher(GeneralUser initiator)
+		{
+			if (initiator == null)
+			{
+				throw new NullReferenceException("Initiatior cannot be null");
+			}
+			_initiator = initiator;
+		}
+
+		public List<Project> GetMatchingProvider(List<Project> searchedProjects)
+		{
+			List<_projectScored> scoredList = new List<_projectScored>();
+			foreach (Project searchedProject in searchedProjects)
+			{
+				int searchedScore = 0;
+				foreach (ProjectFOI searchedFoi in searchedProject.FoiList)
+				{
+					if (_initiator.Interests.Any(i => i.Foi.Id == searchedFoi.Foi.Id))
+					{
+						searchedScore++;
+					}
+				}
+				if (searchedScore > 0)
+				{
+					scoredList.Add(new _projectScored { project = searchedProject, score = searchedScore });
+				}
+			}
+			List<Project> returnList = new List<Project>();
+			foreach (_projectScored scored in scoredList.OrderByDescending(x => x.score))
+			{
+				returnList.Add(scored.project);
+			}
+			return returnList;
+		}
+
+		#region inner object
+		private class _projectScored
+		{
+			public int score;
+			public Project project;
+		}
+		#endregion
+
+		#region fields
+		GeneralUser _initiator;
+
+		#endregion
 	}
 }
