37dac9a1d4d665e8cc30fb971d25671ab2b8b7a6
igor <igor.bedek@gmail.com>
2015-10-11 01:26:33 +0200

    new user-cp

--- Schegrt/WebApp/js/src/controller.user.js
+++ Schegrt/WebApp/js/src/controller.user.js
@@ -6,13 +6,13 @@
 		$scope.company = {};
 		$scope.companyInterests = [];
 
-		$http.get('/api/Providers/1').then(function (response) {
+		$http.get('/api/Providers').then(function (response) {
 			$scope.company = response.data;
 		});
 
-		$http.get('/api/interests/1').then(function (response) {
-			$scope.companyInterests = response.data;
-		})
+		// $http.get('/api/interests/1').then(function (response) {
+		// 	$scope.companyInterests = response.data;
+		// })
 
 
 	});
