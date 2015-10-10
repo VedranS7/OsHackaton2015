71209d4c5b9a343c9c46ee293c8dfc7c6aeff7e2
igor <igor.bedek@gmail.com>
2015-10-10 22:29:37 +0200

    user.js

---
+++ Schegrt/WebApp/js/inc/user.js
@@ -0,0 +1,12 @@
+(function (global) {
+	'use strict';
+
+	var controller = ['$scope', '$http'];
+	controller.push(function ($scope, $http) {
+		
+	});
+
+	angular.module('app.global')
+		.controller('UserController', controller);
+
+}(this) /* Auto-invoked */ );
