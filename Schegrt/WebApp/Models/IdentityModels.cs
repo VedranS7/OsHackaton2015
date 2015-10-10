ef565300e63b345a329cde955f4e2959bb1b56a3
VedranS7 <vedran.sikic@farmeron.com>
2015-10-10 12:27:38 +0200

    Set up connection string

--- Schegrt/WebApp/Models/IdentityModels.cs
+++ Schegrt/WebApp/Models/IdentityModels.cs
@@ -7,9 +7,9 @@ using Microsoft.AspNet.Identity.EntityFramework;
 namespace WebApp.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-    public class ApplicationUser : IdentityUser
-    {
-        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+    public class StudentUser : IdentityUser
+    { 
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<StudentUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
@@ -18,7 +18,18 @@ namespace WebApp.Models
         }
     }
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    public class ProviderUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ProviderUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<StudentUser>
     {
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
