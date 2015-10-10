525f716785b45e220e12bcdae49018129b8c0695
Ivan Kvolik <ivan.kvolik@gmail.com>
2015-10-10 21:29:23 +0200

    Addded provider matcher

---
+++ Schegrt/WEbAppTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApp.Models;
+using System.Collections.Generic;
+
+namespace WEbAppTests
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void ProviderMatcher_MatchingTest()
+		{
+			//Arrange
+			GeneralUser initiator = new StudentUser();
+			//initiator.Interests = new List<>
+		}
+	}
+}
---
+++ Schegrt/WebApp/Models/Matchers/ProviderMatcher.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApp.Models;
+using WebApp.Models.Field;
+
+namespace WebApp.Models.Matchers
+{
+	public class ProviderMatcher
+	{
+		public ProviderMatcher(GeneralUser initiator)
+		{
+			if (initiator == null)
+			{
+				throw new NullReferenceException("Student cannot be null");
+			}
+			_initiator = initiator;
+		}
+
+		public List<GeneralUser> GetMatchingProvider(List<GeneralUser> searchedUsers)
+		{
+			List<_generalScored> scoredList = new List<_generalScored>();
+			foreach (GeneralUser searchedUser in searchedUsers)
+			{
+				int searchedScore = 0;
+				foreach (UserFOI searchedFoi in searchedUser.Interests)
+				{
+					if(_initiator.Interests.Any(i=> i.Id == searchedFoi.Id))
+					{
+						searchedScore++;
+					}
+				}
+				scoredList.Add(new _generalScored {user = searchedUser,score = searchedScore });
+			}
+			List<GeneralUser> returnList = new List<GeneralUser>();
+			foreach(_generalScored scored in scoredList.OrderByDescending(x=>x.score))
+			{
+				returnList.Add(scored.user);
+			}
+			return returnList;
+		}
+
+		#region inner object
+		private class _generalScored
+		{
+			public int score;
+			public GeneralUser user;
+		}
+		#endregion
+
+		#region fields
+		GeneralUser _initiator;
+
+		#endregion
+	}
+}
