cfa1fb018c22a6bf12879ef440e23becb0c7f671
igor <igor.bedek@gmail.com>
2015-10-10 16:56:52 +0200

    register form with account type

--- Schegrt/WebApp/js/src/controller.login.js
+++ Schegrt/WebApp/js/src/controller.login.js
@@ -8,6 +8,13 @@
 		$scope.loginClick = function () {
 			$scope.showLoginForm = !$scope.showLoginForm;
 		};
+
+		$scope.authTypeClick = function (e) {
+			var $this = angular.element(e.currentTarget);
+
+			$this.parent().find('label').removeClass('active');
+			$this.addClass('active');
+		};
 	});
 
 	angular.module('app.global')
