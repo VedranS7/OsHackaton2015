281c57ab6fd77e1cd0ffe5a5602b1bd4b8551941
Ivan Kvolik <ivan.kvolik@gmail.com>
2015-10-10 13:27:02 +0200

    Added Field of interest
    Added Field of interest

---
+++ Schegrt/WebApp/Models/FieldOfInterest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models.Field
+{
+
+	public class FieldOfInterest
+	{
+		public string name
+		{
+			get; set;
+		}
+		public SkillLevel skill
+		{
+			get; set;
+		}
+	}
+}
