9f2a8798357815c59d7f24fd74bda08988b0e7c8
VedranS7 <vedran.sikic@farmeron.com>
2015-10-10 13:52:20 +0200

    Added fields

--- Schegrt/WebApp/Models/DatabaseContext.cs
+++ Schegrt/WebApp/Models/DatabaseContext.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using WebApp.Models.Field;
 
 namespace WebApp.Models
 {
     public class DatabaseContext : DbContext
     {
+        public DbSet<FieldOfInterest> Fields { get; set; }
         public DatabaseContext() : base("DefaultConnection") { }
     }
 }
