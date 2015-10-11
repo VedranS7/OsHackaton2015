6366fb3894a5c25a6338fd2cb6bcc8276c92160c
Ivan Kvolik <ivan.kvolik@gmail.com>
2015-10-11 03:19:37 +0200

    Modified AccountController and logi partial to statics

--- Schegrt/WebApp/Controllers/AccountController.cs
+++ Schegrt/WebApp/Controllers/AccountController.cs
@@ -182,9 +182,11 @@ namespace WebApp.Controllers
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
-
-                    return RedirectToAction("Index", "Home");
-                }
+					if (user.Type == 1)
+	                    return RedirectToAction("Index", "staticstudent");
+					else
+						return RedirectToAction("Index", "staticprovider");
+				}
                 AddErrors(result);
             }
 
